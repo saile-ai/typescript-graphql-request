294db2c1e74f4da61b107aaf339df3afc8ae5be1
Eric McGary <mcgaryes@gmail.com>
2024-07-10 17:00:35 -0400

    wip

--- index.d.ts
+++
@@ -1,6 +0,0 @@
-import { PluginFunction, PluginValidateFn } from '@graphql-codegen/plugin-helpers';
-import { RawGraphQLRequestPluginConfig } from "./config";
-import { GraphQLRequestVisitor } from "./visitor";
-export declare const plugin: PluginFunction<RawGraphQLRequestPluginConfig>;
-export declare const validate: PluginValidateFn<any>;
-export { GraphQLRequestVisitor };
